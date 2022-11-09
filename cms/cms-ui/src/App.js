a050e0fb82f66bd8e9b1fc0596f88426fd33ca37
Alexis Corbacho Golmar <alexis.corbacho@plainconcepts.com>
2022-11-09 13:53:38 +0100

    feat: add link paths to get to restaurant editor

--- cms/cms-ui/src/App.js
+++ cms/cms-ui/src/App.js
@@ -3,20 +3,36 @@ import './App.css';
 import {
   createBrowserRouter,
   RouterProvider,
+  Link
 } from "react-router-dom";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <div>
+      <h3>Welcome to restaurant updater</h3>
+      <Link to="/restaurant/edit">go to restaurant editor</Link>
+      </div>,
+  },
+  {
+    path: "/restaurant/edit",
+    element:<RestaurantEditor></RestaurantEditor>,
   },
 ]);
 
+function RestaurantEditor() {
+  return (
+    <div>
+      <header>wellcome from restaurant editor!</header>
+    </div>
+  )
+}
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        CMS UII
+    <div>
+      <header>
+        CMS UI
       </header>
       <RouterProvider router={router} />
     </div>
